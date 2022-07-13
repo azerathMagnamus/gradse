a7416b0ce815d83bbc0b74dcb0040e1f0295090c
azerathMagnamus <108755525+azerathMagnamus@users.noreply.github.com>
2022-07-13 17:20:37 -0600

    Update main.js

--- main.js
+++ main.js
@@ -96,9 +96,9 @@ function countBelow50() {
 }
 
 function lowGradesTo50() {
-  for(let n=0;n<grades.length;n++ )
-  if (grades[n]<50){
-    grades[n]=50
+  last = grades.length;
+  lastgrade = last - 1;
+  grades[lastgrade] = 50;
   }
   // Change all grades that are below 50 to be equal to 50.
   outputEl.innerHTML = "Change low grades to 50";
